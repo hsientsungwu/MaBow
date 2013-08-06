94f80dc2135d7e7385b33bffc86f1cc6b497044b
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-08-05 23:08:45 -0400

    mobile frontend templates

---
+++ test/jquerymobile.test.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Test Jquery Mobile</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	</head>
+	<body>
+		<div data-role="header">
+		    <h1>媽寶 - 線上影音</h1>
+		</div>
+		<ul data-role="listview" data-inset="true">
+		    <li><a href="jquerymobile2.test.php" data-transition="slide" data-inline="true">綜藝節目</a></li>
+		    <li><a href="#">談話節目</a></li>
+		    <li><a href="#">政論節目</a></li>
+		    <li><a href="#">台灣偶像劇</a></li>
+		</ul>
+	</body>
+</html>
---
+++ test/jquerymobile2.test.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Test Jquery Mobile</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	</head>
+	<body>
+		<div data-role="header">
+			<a href="jquerymobile.test.php" data-icon="back" data-transition="slide" data-direction="reverse">上一頁</a>
+		    <h1>媽寶 - 線上影音</h1>
+		</div>
+		<ul data-role="listview" data-inset="true">
+		    <li><a href="video.html" data-transition="slide" data-inline="true">111</a></li>
+		    <li><a href="#">222</a></li>
+		    <li><a href="#">333</a></li>
+		    <li><a href="#">444</a></li>
+		</ul>
+	</body>
+</html>
