c457cf3d4b66aa730d89e502160a68bc18f45102
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-08-21 22:26:31 -0400

    moving ga to footer

--- frontend/templates/header.template.php
+++ frontend/templates/header.template.php
@@ -3,16 +3,6 @@
     <title><?php echo ($pageTitle ? $pageTitle : '媽寶 - 線上影音'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-43322426-3', 'htwu.net');
-	  ga('send', 'pageview');
-
-	</script>
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
