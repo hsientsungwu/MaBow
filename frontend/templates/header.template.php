52305ae0b5322c467768bb8ba53ef4535aead3a2
Steve Wu <wu.steve@qgiv.com>
2013-08-28 13:11:51 -0400

    fix the header

--- frontend/templates/header.template.php
+++ frontend/templates/header.template.php
@@ -1,12 +1,6 @@
 <html>
   <head>
     <title><?php echo ($pageTitle ? $pageTitle : '媽寶'); ?></title>
-    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-icon-57x57-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-icon-72x72-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-icon-114x114-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-icon-144x144-precomposed.png" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta property="og:title" content="媽寶-線上影音"/>
     <meta property="og:image" content="http://mabow.htwu.net/img/logoist_logo_nobkg.png" />
@@ -15,17 +9,22 @@
     <meta property="og:description" content="媽寶 - 線上影音網站，年長的使用者不再需要煩惱如何線上觀看影片，不再把身邊的 3C 產品涼在旁邊，打開媽寶，開始您的縣上影音旅程！" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-icon-57x57-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-icon-72x72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-icon-114x114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-icon-144x144-precomposed.png" />
     <link rel="stylesheet" href="/css/frontend.css">
     <link rel="stylesheet" href="/css/clank.css">
+    <link rel="stylesheet" href="/css/add2home.css">
     
     <script type="text/javascript" src="/support/components/jquery/jquery.min.js"></script>
     <script type="text/javascript" src="/support/components/prefix-free/prefixfree.min.js"></script>
-
     <script type="text/javascript" src="/js/clank/ratchet/fingerblast.js"></script>
     <script type="text/javascript" src="/js/clank/ratchet/sliders.js"></script>
     <script type="text/javascript" src="/js/clank/ratchet/toggles.js"></script>
     <script type="text/javascript" src="/js/clank/clank.js"></script>
-    <link rel="stylesheet" href="/css/add2home.css">
     <script type="application/javascript" src="/js/add2home.js" charset="utf-8"></script>
     
   </head>
