2c9bbdff5ffeebf956275aa326509d91cc0f79d5
HsienTsung Wu <hsientsungwu@new-host-3.home>
2013-08-29 22:57:36 -0400

    work with iphone 5

--- frontend/templates/header.template.php
+++ frontend/templates/header.template.php
@@ -1,6 +1,13 @@
 <html>
   <head>
     <title><?php echo ($pageTitle ? $pageTitle : '媽寶'); ?></title>
+    <!-- standard viewport tag to set the viewport to the device's width
+    , Android 2.3 devices need this so 100% width works properly and
+    doesn't allow children to blow up the viewport width-->
+<meta name="viewport" id="vp" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+<!-- width=device-width causes the iPhone 5 to letterbox the app, so
+    we want to exclude it for iPhone 5 to allow full screen apps -->
+<meta name="viewport" id="vp" content="initial-scale=1.0,user-scalable=no,maximum-scale=1" media="(device-height: 568px)" />
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-icon-57x57-precomposed.png" />
@@ -13,7 +20,6 @@
     <meta property="og:url" content="http://mabow.htwu.net"/>
     <meta property="og:site_name" content="媽寶-線上影音"/>
     <meta property="og:description" content="媽寶 - 線上影音網站，年長的使用者不再需要煩惱如何線上觀看影片，不再把身邊的 3C 產品涼在旁邊，打開媽寶，開始您的縣上影音旅程！" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="stylesheet" href="/css/frontend.css">
     <link rel="stylesheet" href="/css/clank.css">
@@ -27,7 +33,9 @@
     <script type="text/javascript" src="/js/clank/clank.js"></script>
     <link rel="stylesheet" href="/css/add2home.css">
     <script type="application/javascript" src="/js/add2home.js" charset="utf-8"></script>
-    
+    <script>
+    (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
+    </script>
   </head>
   <body data-os="ios">
         <div class="cl-device-body">
