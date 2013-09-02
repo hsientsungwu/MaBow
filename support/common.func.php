53cbaeb98fb501fe57f91b38e6135668a9e8912f
HsienTsung Wu <hsientsungwu@new-host-2.home>
2013-09-01 22:32:56 -0400

    Update email content type

--- support/common.func.php
+++ support/common.func.php
@@ -35,7 +35,7 @@ function send_email($content) {
 
 	// To send HTML mail, the Content-type header must be set
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=Big5' . "\r\n";
 
 	// Additional headers
 	$headers .= 'To: HTWU Webmaster<' . $ADMIN_EMAIL . '>' . "\r\n";
